3a8a221169627e84e469a6cd79a9a6d356265008
Andres Romero <me@afromero.co>
2025-01-22 12:01:46 +0100

    add manual seed to cuda

--- pixelcache/tools/utils.py
+++ pixelcache/tools/utils.py
@@ -66,6 +66,8 @@ def seed_everything(seed: int | None = None, *, workers: bool = False) -> int:
     random.seed(seed)
     np.random.seed(seed)  # noqa: NPY002
     torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
     os.environ["PL_SEED_WORKERS"] = f"{int(workers)}"
 
     return seed
